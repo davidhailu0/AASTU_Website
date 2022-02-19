93006cc5682ebf1f801c03ac98dbd7492f0a8cfb
Dawit Hailu Birhane <Davidhailu0@gmail.com>
2022-02-19 08:23:23 -0800

    modify contact us and our partners pages

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -6,22 +6,13 @@ import Location from '@mui/icons-material/MyLocation'
 import Button from '@mui/material/Button';
 import { Waypoint } from 'react-waypoint';
 import $ from 'jquery';
-import Wave from 'react-wavify'
+import videoCT from '../../videos/contactUs.mp4';
 import aastuMap from '../../images/aastu_map.png';
 import './ContactUs.css';
 export default function ContactUs(){
     const aastuMapUrl = 'https://www.google.com/maps/search/AASTU/@8.8863123,38.807744,884m/data=!3m2!1e3!4b1?hl=am';
     return (<section id='Contact_Us' className='Contact_Us animate__slideInRight'>
-        <Waypoint onEnter={()=>{
-            $('.row1').addClass('animate__slideOutRight');
-            $('.row2').addClass('animate__slideOutRight');
-        }} onLeave={()=>{
-            $('.row1').removeClass('animate__slideOutRight');
-            $('.row2').removeClass('animate__slideOutRight');
-             $('.row1').addClass('animate__slideInRight');
-            $('.row2').addClass('animate__slideInRight');
-        }}/>
-       <Wave mask="url(#mask)" fill="#1277b0" >
+       {/* <Wave mask="url(#mask)" fill="#1277b0" >
   <defs>
     <linearGradient id="gradient" gradientTransform="rotate(90)">
       <stop offset="0" stopColor="white" />
@@ -31,14 +22,15 @@ export default function ContactUs(){
       <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)"  />
     </mask>
   </defs>
-</Wave>
+</Wave> */}
+        <video src={videoCT} autoPlay loop muted/>
         <Typography sx={{textAlign:'center',fontWeight:'bold',mb:'2rem'}} variant='h2'>Contact Us</Typography>
          <Box sx={{display:{md:'flex',xs:'grid'},justifyContent:'space-evenly',px:'2rem'}}>
-        <Box sx={{flexBasis:'30%'}} className='address animate__animated animate__delay-1s'>
-        <Typography sx={{color:'black'}}><Tel sx={{pr:'1rem'}}/>Tel: +251 11 888 0610</Typography>
-        <Typography sx={{color:'black'}}><Mobile sx={{pr:'1rem'}}/>Mob:+251 993 - 80 71 78</Typography>
-        <Typography sx={{color:'black'}}><Mail sx={{pr:'1rem'}}/>E-mail: pir@aastu.edu.et</Typography>
-        <Typography sx={{color:'black'}}><Location sx={{pr:'1rem'}}/>Addis Ababa, Ethiopia</Typography>
+        <Box sx={{flexBasis:'30%',ml:'4rem'}} className='address animate__animated animate__delay-1s'>
+        <Typography sx={{color:'white'}}><Tel sx={{pr:'1rem',color:'white'}}/>Tel: +251 11 888 0610</Typography>
+        <Typography sx={{color:'white'}}><Mobile sx={{pr:'1rem',color:'white'}}/>Mob:+251 993 - 80 71 78</Typography>
+        <Typography sx={{color:'white'}}><Mail sx={{pr:'1rem',color:'white'}}/>E-mail: pir@aastu.edu.et</Typography>
+        <Typography sx={{color:'white'}}><Location sx={{pr:'1rem',color:'white'}}/>Addis Ababa, Ethiopia</Typography>
         </Box>
         {/* <div style={{flexBasis:'30%'}} dangerouslySetInnerHTML={{__html:'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4213.975453195402!2d38.8075876147182!3d8.885124593620771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b82a7e392203f%3A0xb05f440eacc98f9f!2sAASTU!5e1!3m2!1sam!2set!4v1643722845315!5m2!1sam!2set" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'}}></div>     */}
         <Box sx={{flexBasis:'30%'}} className='map animate__animated animate__delay-1s'>
@@ -46,7 +38,7 @@ export default function ContactUs(){
                  window.location.href = aastuMapUrl;
             }}/>
         </Box>
-        <Box component="form" noValidate sx={{ mt: 1,ml:'1rem',flexBasis:'32%' }} className='form-container animate__animated animate__delay-1s'>
+        <Box component="form" noValidate sx={{ mt: 1,ml:'1rem',flexBasis:'32%',mr:'1rem' }} className='form-container animate__animated animate__delay-1s'>
             <input type="text" placeholder='Enter Your Name' required/>
             <input type="email" placeholder='Enter Your Email Address' required/>
             <input type="text" placeholder='Enter Your Subject' required/>
--- src/components/OurPartners/index.js
+++ src/components/OurPartners/index.js
@@ -4,20 +4,34 @@ import apple from '../../images/Apple.png';
 import microsoft from '../../images/microsoft.png';
 import tesla from '../../images/tesla.png';
 import johnson from '../../images/Johnson.png';
+import ibm from '../../images/IBM.jpg';
+import { Waypoint } from 'react-waypoint';
+import $ from 'jquery';
+import amazon from '../../images/amazon.png';
+import cloudera from '../../images/cloudera.png';
 import './ourPartners.css';
 export default function OurPartners(){
     return (<section id='Our_Partners' className='our_partners'>
+        <Waypoint onEnter={()=>{
+            $('.row1').addClass('animate__slideInRight animate__delay-1s');
+            $('.row2').addClass('animate__slideInRight animate__delay-1s');
+        }}/>
         <Typography sx={{textAlign:'center',fontWeight:'bold',mb:'0.5rem'}} variant='h2'>Our Partners</Typography>
         <hr style={{width:'100px',marginBottom:'2rem',color:'#1067CC'}}/>
         <Box sx={{display:{xs:'grid',md:'flex'},justifyContent:'space-evenly',alignItems:'center',rowGap:'2rem',mb:'3rem'}} className='animate__animated row1'>
-            <Box><img src={google} alt="google" /></Box>
-            <Box><img src={apple} alt="apple" /></Box>
-            <Box><img src={johnson} alt="dell"/></Box>
+            <Box><img src={google} height='100' alt="google" /></Box>
+            <Box><img src={apple} height='100' alt="apple" /></Box>
+            <Box><img src={johnson} height='100' alt="dell"/></Box>
         </Box>
         <Box sx={{display:{xs:'grid',md:'flex'},justifyContent:'space-evenly',alignItems:'center',rowGap:'2rem',}} className='animate__animated row2'>
-            <Box><img src={microsoft} alt="microsoft" /></Box>
-            <Box><img src={tesla} alt="tesla" /></Box>
-            <Box><img src={johnson} alt="pepsi" /></Box>
+            <Box><img src={microsoft} height='100' alt="microsoft" /></Box>
+            <Box><img src={tesla} height='100' alt="tesla" /></Box>
+            <Box><img src={johnson} height='100' alt="johnson" /></Box>
+        </Box>
+        <Box sx={{display:{xs:'grid',md:'flex'},justifyContent:'space-evenly',alignItems:'center',rowGap:'2rem',mt:'4rem'}} className='animate__animated row2'>
+            <Box sx={{ml:'3rem'}}><img src={ibm} height='100' alt="microsoft" /></Box>
+            <Box><img src={amazon} height='100' alt="tesla" /></Box>
+            <Box><img src={cloudera} height='100' alt="johnson" /></Box>
         </Box>
     </section>);
 }
